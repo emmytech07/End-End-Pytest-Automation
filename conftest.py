e27312313dc582f87f9b8375d1d4bd566528ad32
emmytech07 <ilesanmiemmanuel07@gmail.com>
2022-09-25 13:27:20 +0100

    adding driver

--- conftest.py
+++ conftest.py
@@ -7,14 +7,29 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+def pytest_addoption(parser):
+    parser.addoption(
+        "--browser_name", action="store", default="chrome"
+    )
+
 
 @pytest.fixture(scope="class")
 def setup(request):
-    service_obj = Service("D:/chromedriver/chromedriver")
-    driver = webdriver.Chrome(service=service_obj)
-    driver.implicitly_wait(5)
-    driver.maximize_window()
-    driver.get("http://www.rahulshettyacademy.com/angularpractice/")
+    brower_name = request.config.getOption("browser_name")
+
+    if brower_name == "chrome":
+        service_obj = Service("D:/chromedriver/chromedriver")
+        driver = webdriver.Chrome(service=service_obj)
+        driver.implicitly_wait(5)
+        driver.maximize_window()
+        driver.get("http://www.rahulshettyacademy.com/angularpractice/")
+
+    elif brower_name == "firefox":
+        pass
+
+    elif brower_name == "IE":
+        pass
+
     request.cls.driver = driver
     yield
     driver.close()
